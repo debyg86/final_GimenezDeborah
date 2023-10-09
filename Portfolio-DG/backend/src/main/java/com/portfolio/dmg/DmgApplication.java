1a7f44ecfaf5e5348b5b866accca1f6ae78e5841
debyg86 <debyg86@gmail.com>
2023-10-08 22:38:02 -0300

    10-10

--- Portfolio-DG/backend/src/main/java/com/portfolio/dmg/DmgApplication.java
+++ Portfolio-DG/backend/src/main/java/com/portfolio/dmg/DmgApplication.java
@@ -1,13 +1,13 @@
-package com.portfolio.mgb;
+package com.portfolio.dmg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MgbApplication {
+public class DmgApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MgbApplication.class, args);
+		SpringApplication.run(DmgApplication.class, args);
 	}
 
 }
